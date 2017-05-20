7ce08d788ad8d4dc4efcd7bf19261ca9afe083f6
sidharth-mishra <mishrasidhartha4@gmail.com>
2017-05-20 10:51:56 +0530

    Initial Commit

---
+++ src/com/test/PrimeNumber.java
@@ -0,0 +1,25 @@
+package com.test;
+
+public class PrimeNumber {
+	
+	
+public static void main(String[] args) {
+	
+	System.out.println(isPrime(8));
+	
+}
+
+	public static boolean isPrime(int number){
+		boolean flag=true;
+		for(int i=2;i<number/2;i+=2){
+			if(number%i==0 || number%(i+1)==0){
+				flag=false;
+				break;
+			}
+		}
+		
+		return flag;
+	}
+	
+	
+}
---
+++ src/com/test/Starprint.java
@@ -0,0 +1,32 @@
+package com.test;
+
+public class Starprint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		printStar(7);
+		
+		
+	}
+	public static void printStar(int number){
+		
+		for(int i=0;i<=number;i++){
+			System.out.println(" ");
+
+			for(int j=i+1;j<=number;j++){
+				System.out.print(" ");
+			}
+			
+			for(int k=1;k<=i+1;k++){
+				System.out.print(" *");
+			}
+
+		}
+		
+		
+		
+	}
+
+}
