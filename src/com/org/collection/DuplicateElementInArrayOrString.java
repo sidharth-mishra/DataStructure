cb50bfd2027a6a576e81fdccd0b2e63666208464
sidharth-mishra <mishrasidhartha4@gmail.com>
2017-05-23 00:51:50 +0530

    New Class added

---
+++ src/com/org/collection/DuplicateElementInArrayOrString.java
@@ -0,0 +1,42 @@
+package com.org.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DuplicateElementInArrayOrString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String x="papu is a good boy";
+System.out.println(getFirstNonDuplicateCharacter(x));
+	}
+	
+	
+	public static char getFirstNonDuplicateCharacter(String word){
+		
+char[]  stringData=word.toCharArray();
+Map<Character,Integer> mp=new HashMap<Character,Integer>();
+for(int i=0;i<stringData.length;i++){
+	
+	if(mp.containsKey(stringData[i])){
+		mp.put(stringData[i], mp.get(stringData[i])+1);
+	}
+	else{
+		mp.put(stringData[i], 1);
+	}
+}
+
+		
+		
+	
+		
+for(int i=0;i<stringData.length;i++)	{
+	if(mp.get(stringData[i])==1){
+		return stringData[i];
+	}
+}
+		return 0;
+		
+	}
+
+}
